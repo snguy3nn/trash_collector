79280911c3cab51524c5226b67b27f2131b180eb
Stephen Nguyen <snguy3nn@gmail.com>
2021-06-04 14:04:45 -0500

    Created properties for employee model

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -4,3 +4,8 @@ from django.db import models
 
 # TODO: Create an Employee model with properties required by the user stories
 
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    zip_code = models.CharField(max_length=50)
