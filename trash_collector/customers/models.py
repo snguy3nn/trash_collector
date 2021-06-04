50a50cec2fdbc3f51e710fe3022fae521a51aa7f
Stephen Nguyen <snguy3nn@gmail.com>
2021-06-04 13:58:44 -0500

    Created properties for customer model

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -7,3 +7,11 @@ from django.db import models
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
+    pickup_day = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=50)
+    balance = models.IntegerField(default=0)
+    one_time_pickup = models.DateField(null=True)
+    suspension_start = models.DateField(null=True)
+    suspension_end = models.DateField(null=True)
+
--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -3,3 +3,7 @@ from django.db import models
 # Create your models here.
 
 # TODO: Create an Employee model with properties required by the user stories
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE
