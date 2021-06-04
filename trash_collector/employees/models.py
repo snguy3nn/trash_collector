de6807c5d71dc4125bf72ffba186eac6b54e0048
Stephen Nguyen <snguy3nn@gmail.com>
2021-06-04 14:01:28 -0500

    Created properties for customer model

--- trash_collector/employees/models.py
+++ trash_collector/employees/models.py
@@ -4,6 +4,3 @@ from django.db import models
 
 # TODO: Create an Employee model with properties required by the user stories
 
-class Employee(models.Model):
-    name = models.CharField(max_length=50)
-    user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE
