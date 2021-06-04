632ee80396f5e0453603679e026afa9d1204c2e0
ArthurD25 <arthuro.oneal@gmail.com>
2021-06-04 15:25:46 -0400

    changes to customer and employee models.

--- trash_collector/customers/models.py
+++ trash_collector/customers/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 # TODO: Finish customer model by adding necessary properties to fulfill user stories
 
-
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     user = models.ForeignKey('accounts.User', blank=True, null=True, on_delete=models.CASCADE)
--- trash_collector/trash_collector/settings.py
+++ trash_collector/trash_collector/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'customers',
-    'employees'
+    'customers.app.CustomersConfig',
+    'employees.app.EmployeesConfig'
 ]
 
 MIDDLEWARE = [
